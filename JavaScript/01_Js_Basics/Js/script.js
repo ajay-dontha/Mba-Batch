425fe039105933b8e8cbb555331206b3dafa6026
ajay-developer <84758365+ajay-dontha@users.noreply.github.com>
2023-01-02 18:23:12 +0530

    Add files via upload

---
+++ JavaScript/01_Js_Basics/Js/script.js
@@ -0,0 +1,3 @@
+var firstName = "Happy New Year";
+var lastName = 2023;
+console.log(firstName+lastName);
---
+++ JavaScript/03_Js_Console_Statements/script.js
@@ -0,0 +1,5 @@
+console.log("to print outputs");
+console.info("info type of messages");
+console.warn("this is warning message");
+console.error("this is error message");
+console.table("to print table");
